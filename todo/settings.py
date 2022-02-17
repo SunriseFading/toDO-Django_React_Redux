ae1f76a797f3770f71de9e6746877b8383a8c476
SunriseFading <vlad.bykov2004@mail.ru>
2022-02-18 01:47:59 +0300

    Add react

--- todo/settings.py
+++ todo/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main.apps.MainConfig',
+    'frontend.apps.FrontendConfig',
     'rest_framework',
-    'main.apps.MainConfig'
 ]
 
 MIDDLEWARE = [
