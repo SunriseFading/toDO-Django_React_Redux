8b0924aad8869d6017341594b6a8b6017aa7c115
SunriseFading <vlad.bykov2004@mail.ru>
2022-02-18 22:31:39 +0300

    Add React in app

---
+++ frontend/src/components/todo/Form.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Form extends Component {
+    render() {
+        return <div>
+            Add toDO form
+        </div>;
+    }
+}
+
+export default Form;
